9cada4282fda9e38d64fbf7e5fc3c17ebd58fdf2
seokmin <seokmin@chain.partners>
2019-01-01 22:36:19 +0900

    Add test db migrations for use

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from recorder import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-migrate = Migrate(app, db)
+from recorder import app, db
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
 manager.add_command('db', MigrateCommand)
 
 manager.run()
