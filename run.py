16e21351404daa816225d6d1ab7a79b306b52fbb
seokmin <seokmin@chain.partners>
2019-01-01 21:59:29 +0900

    Add docker-compose and postgresql setting for db

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 from recorder import app
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-app.run(debug=True)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+manager.run()
