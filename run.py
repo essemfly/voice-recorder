0159e75844301afe475a962d9a7e23073c7437f3
seokmin <seokmin@chain.partners>
2019-01-01 22:28:39 +0900

    Add migration files for db

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-from recorder import app
-from flask_sqlalchemy import SQLAlchemy
+from recorder import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
