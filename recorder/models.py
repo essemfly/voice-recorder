3216b628136c44cc18a66931416cbec7b097c428
seokmin <seokmin@chain.partners>
2019-01-15 00:51:54 +0900

    Add migrations for users

--- recorder/models.py
+++ recorder/models.py
@@ -1,7 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(128), unique=True)
+    passwd_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.passwd_hash, password)
