468ba30138f56b82ec89fb848552243c4ac64de4
seokmin <seokmin@chain.partners>
2019-01-15 01:57:29 +0900

    Add registration page for signup

--- recorder/routes.py
+++ recorder/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from recorder import app
+from recorder import app, db
 from recorder.models import User
-from recorder.forms import LoginForm
+from recorder.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
@@ -29,6 +29,22 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/logout')
 def logout():
