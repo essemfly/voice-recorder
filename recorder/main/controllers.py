96aac3ab3893742772583365609a46576496b43d
seokmin <seokmin@chain.partners>
2019-02-10 21:16:48 +0900

    Make post call not to make error on success

--- recorder/main/controllers.py
+++ recorder/main/controllers.py
@@ -2,7 +2,7 @@
 
 import os.path
 import datetime
-from flask import render_template, request, current_app, redirect, url_for, json
+from flask import render_template, request, current_app, redirect, url_for, json, jsonify
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 from recorder import basedir, db
@@ -27,6 +27,7 @@ def record(script_id):
     script = Script.query.get(script_id)
     return render_template('/main/record.html', script=script, user=current_user)
 
+
 @bp.route('/scripts/seq/<int:script_id>')
 def record_seq(script_id):
     script = Script.query.get(script_id)
@@ -37,6 +38,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @bp.route('/voices/<int:user_id>')
 def user(user_id):
     voices = Voice.query.filter_by(user_id=user_id).all()
@@ -44,17 +46,17 @@ def user(user_id):
     seconds = 0
 
     for voice in voices:
-        print('hoit', voice.duration)
         voice_temp = {}
         voice_temp["duration"] = voice.duration.total_seconds()
         voice_temp["sentence"] = voice.sentence
         voice_temp["filename"] = voice.filename
-        voice_temp["created_at"] = voice.created_at.replace(microsecond=0).isoformat()
+        voice_temp["created_at"] = voice.created_at.replace(
+            microsecond=0).isoformat()
         voices_json.append(voice_temp)
         seconds += voice.duration.total_seconds()
-    
-    print(seconds)
+
     return render_template('/main/voice.html', voices=json.dumps(voices_json, ensure_ascii=False), total_seconds=seconds)
+
 
 @bp.route('/voice/<int:user_id>/<int:script_id>', methods=['GET', 'POST'])
 def upload_file(user_id, script_id):
@@ -74,7 +76,8 @@ def upload_file(user_id, script_id):
             user = User.query.get(user_id)
             file.save(os.path.join(
                 basedir, current_app.config['UPLOAD_FOLDER'] + "/" + user.username, filename))
-            voice = Voice(filename = filename, sentence= sentence, duration=duration, script_id=script_id, user_id=user_id)
+            voice = Voice(filename=filename, sentence=sentence,
+                          duration=duration, script_id=script_id, user_id=user_id)
             db.session.add(voice)
             db.session.commit()
-            return redirect(url_for('main.index'))
+            return jsonify(success=True)
