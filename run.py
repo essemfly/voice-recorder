a49343b1658aad16198855ce553235a2e21f0c44
seokmin <seokmin@chain.partners>
2019-02-12 20:25:42 +0900

    Add generate json file for recorded audios

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
-from recorder import create_app, db
 from fixtures import fixture
+from recorder import create_app, db
+from recorder.main.models import Voice
+from recorder.auth.models import User
+import json
 
 app = create_app()
 manager = Manager(app)
@@ -18,5 +21,21 @@ def seed_first():
 def seed_second():
     fixture.add_lyrics_2018_first()
 
+
+@manager.command
+def generate_json(username):
+    user = User.query.filter_by(username=username).first()
+    records = Voice.query.filter_by(user_id=user.id).all()
+
+    data = {}
+
+    for record in records:
+        key_name = "./datasets/%s/audio/%s" % (user.username, record.filename)
+        data[key_name] = record.sentence
+
+    with open('./voice_data/%s/alignment.json' % (user.username), 'w') as outfile:
+        json.dump(data, outfile, ensure_ascii=False)
+
+    return "Finished records: %s" % (str(len(records)))
 
 manager.run()
